adc59b5e941b8aa053186d6fb101e18bcec47459
alberto <garar.alberto@gmail.com>
2022-02-28 15:57:43 +0100

    Fix visual representation script and add overlapping counting script

--- scripts/countOverlap.py
+++ scripts/countOverlap.py
@@ -12,18 +12,17 @@ annotationList = os.listdir(annotationsFolder)
 audioFolder = os.path.expanduser("../data/nips4b/audio/original/train/")
 
 # Data frame building
+columnNames = ['Start','Duration', 'Label']
 
 # Iterate each annotation file
 for annotationFile in annotationList:
     fileID = annotationFile.replace("annotation_train", "")
     fileID = fileID.replace(".csv", "")
 
-    columnNames = ['Start','Duration', 'Label']
     annotFile = os.path.join(annotationsFolder, annotationFile)
     df = pd.read_csv(annotFile, names = columnNames)
 
     megaList = []
-
     # Check every entry in annotation file
     for i in range(len(df)):
         roundedStart = math.floor(df['Start'][i] * 100)/100.0
@@ -49,7 +48,7 @@ for annotationFile in annotationList:
         if label not in allLabels:
             allLabels.append(label)
 
-    # Get timestamps and create empty(0) 'binary' matrix
+    # Modify matrix to values in classes
     timeStamps = np.arange(0, duration, 0.01).tolist()
     timeStamps = [round(i, 2) for i in timeStamps]
     detectionMatrix = np.zeros(shape= (len(allLabels), len(timeStamps)))
@@ -67,25 +66,44 @@ for annotationFile in annotationList:
 
                 # Obtain values (1, 2, 3,...) of the species and put them in their corresponding row
                 for time in tVals:
-                    timeIndex = int(round(100*float(time)))-1
+                    timeIndex = int(float(time)/0.01) - 1
                     
-                    emptyRow[timeIndex] = allLabels.index(label)+1
+                    emptyRow[timeIndex] = int(allLabels.index(label)+1)
         
             detectionMatrix[allLabels.index(label)] = emptyRow
 
-    ###########################################  PLOTTING  ###########################################
+###########################################  GET OVERLAPPING  ###########################################
 
+    # Column headers
     timeStr = [str(x) for x in timeStamps]
     pltHeaders = timeStr
 
+    # Create dataframe
     dfFinal = pd.DataFrame(detectionMatrix, columns=pltHeaders)
     dfFinal.index = allLabels
     dfFinal.index.name = 'Species'
 
+    # Study overlapping (more than 1 non zero value in a column)
+    overlap = False 
+    maxOverlap = 1
+    timesOverlap = [] # Number of overlapping   
+    colIndex = 0
+    for column in dfFinal:
+        events = []
+        for val in dfFinal[column]:
+            if val != 0: events.append(val) 
 
-    print("Creating detection plot for file {:s}... ".format(fileID))
-    plt.figure()
-    sns.heatmap(dfFinal)
-    #plt.show()
-    fName = '../data/nips4b/visualAnnotations/'+"visualAnnotation_"+fileID+".png"
-    plt.savefig(fName)
+        # General overlap
+        if len(events) > 1: 
+            overlap = True
+            timesOverlap.append(colIndex*0.01)
+        # Max overlap
+        if (len(events) > maxOverlap): maxOverlap = len(events)
+
+        colIndex += 1
+   
+    print("\nOverlapping study of audio file {:d}: ".format(int(fileID)))
+    print("Appearing species: ", len(allLabels), "(", allLabels,")")
+    print("Is there overlapping? ", overlap)
+    print("Maximum overlapping: ", maxOverlap)
+    print("Overlapping percentage: ", 100*len(timesOverlap)/len(timeStamps))
