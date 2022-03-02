e83224d7dda3bd283d91dd3a59d277af90d245fa
alberto <garar.alberto@gmail.com>
2022-03-02 16:51:56 +0100

    Improve synthesis and visual annotation options

--- scripts/getVisualAnnot.py
+++ scripts/getVisualAnnot.py
@@ -5,23 +5,33 @@ import librosa
 import matplotlib.pyplot as plt
 import math 
 import seaborn as sns
+import sys
 
 # Paths 
-annotationsFolder = os.path.expanduser("../data/nips4b/annotations/")
+# Arguments:
+# 0.- .py file name 
+# 1.- Audio final folder
+# 2.- Annotations final folder
+argAudio = sys.argv[1]
+argAnnot = sys.argv[2]
+
+pathAudio = "../data/nips4b/audio/"+argAudio
+pathAnnot = "../data/nips4b/"+argAnnot
+
+annotationsFolder = os.path.expanduser(pathAnnot)
 annotationList = os.listdir(annotationsFolder)
-audioFolder = os.path.expanduser("../data/nips4b/audio/original/train/")
+audioFolder = os.path.expanduser(pathAudio)
 
 # Data frame building
 
 # Iterate each annotation file
 for annotationFile in annotationList:
-    fileID = annotationFile.replace("annotation_train", "")
-    fileID = fileID.replace(".csv", "")
+    fileID = annotationFile[:-4]
 
     columnNames = ['Start','Duration', 'Label']
     annotFile = os.path.join(annotationsFolder, annotationFile)
     df = pd.read_csv(annotFile, names = columnNames)
-
+    print(df)
     megaList = []
 
     # Check every entry in annotation file
@@ -36,10 +46,11 @@ for annotationFile in annotationList:
         megaList.append(listTime)
 
     ###################################################
-    audioFile = "nips4b_birds_trainfile"+fileID+".wav"
+    audioFile = fileID+".wav"
     audioFile = os.path.join(audioFolder, audioFile)
     # Check duration of file 
     duration = librosa.get_duration(filename=audioFile) +0.01
+    print(duration)
     # Take only 2 decimals 
     duration = round(duration, 2)
 
@@ -50,7 +61,7 @@ for annotationFile in annotationList:
             allLabels.append(label)
 
     # Get timestamps and create empty(0) 'binary' matrix
-    timeStamps = np.arange(0, duration, 0.01).tolist()
+    timeStamps = np.arange(0, duration+1, 0.01).tolist()
     timeStamps = [round(i, 2) for i in timeStamps]
     detectionMatrix = np.zeros(shape= (len(allLabels), len(timeStamps)))
 
@@ -68,7 +79,7 @@ for annotationFile in annotationList:
                 # Obtain values (1, 2, 3,...) of the species and put them in their corresponding row
                 for time in tVals:
                     timeIndex = int(round(100*float(time)))-1
-                    
+
                     emptyRow[timeIndex] = allLabels.index(label)+1
         
             detectionMatrix[allLabels.index(label)] = emptyRow
@@ -86,6 +97,6 @@ for annotationFile in annotationList:
     print("Creating detection plot for file {:s}... ".format(fileID))
     plt.figure()
     sns.heatmap(dfFinal)
-    #plt.show()
-    fName = '../data/nips4b/visualAnnotations/'+"visualAnnotation_"+fileID+".png"
-    plt.savefig(fName)
+    plt.show()
+    # fName = '../data/nips4b/visualAnnotations/'+"visualAnnotation_"+fileID+".png"
+    # plt.savefig(fName)
--- scripts/synthesis.py
+++ scripts/synthesis.py
@@ -1,21 +1,94 @@
 from pydub import AudioSegment
+import pandas as pd
+import shutil
+
+########################### METHODS ###########################
+
+# Concatenate CSV2 and CSV1
+def getFileID(fileName):
+    fileID = fileName[:-4] # Remove .csv
+    fileID = fileID[-3:] # Remove the rest, keep only numbers(ID)
+    return fileID
+
+def concatCSV(csv1, csv2, position1, position2):
+    id1 = getFileID(csv1)
+    id2 = getFileID(csv2)
+    
+    # Create empty CSV
+    csvName = id1+"_"+id2+".csv"
+  
+    original = csv1
+    target = outAnnotFolder+csvName
+
+    #shutil.copyfile(original, target)
+
+    concatCSV = open(target, "a+")
+
+    # Fill final csv
+    file1 = open(csv1, "r")
+    file2 = open(csv2, "r")
+
+    for line in file1:
+        elems = line.split(",")
+        print(elems[0])
+        line = str(float(elems[0])+float(position1))+","+elems[1]+","+elems[2]
+        
+        concatCSV.write(line)
+    for line in file2:
+        # Change start time depending on position 
+        elems = line.split(",")
+        line = str(float(elems[0])+float(position2))+","+elems[1]+","+elems[2]
+        
+        concatCSV.write(line)
+
+    # Close files
+    file1.close()
+    file2.close()
+    concatCSV.close()
+
+    return csvName
+
+def getCSV_DF(csvFile):
+    columnNames = ['Start','Duration', 'Label']
+    df = pd.read_csv(csvFile, names = columnNames)
+    return df
+
+
+############################ MAIN ############################
 
 audioFolder = "../data/nips4b/audio/original/train/"
 annotFolder = "../data/nips4b/annotations/"
-outFolder = "../data/nips4b/audio/o3/"
 
+outAudioFolder = "../data/nips4b/audio/o3/"
+outAnnotFolder = "../data/nips4b/mergedAnnotations/"
 
-sound1 = AudioSegment.from_file(audioFolder+"nips4b_birds_trainfile149.wav", format="wav")
-sound2 = AudioSegment.from_file(audioFolder+"nips4b_birds_trainfile241.wav", format="wav")
+stdDuration = 10
 
-combined = sound1 + sound2
-sound1 = sound1 + 6
-overlay = sound1.overlay(sound2, position=0)
+sound1 = AudioSegment.from_file(audioFolder+"149.wav", format="wav")
+sound2 = AudioSegment.from_file(audioFolder+"012.wav", format="wav")
+emptyAudio = AudioSegment.silent(duration=1000*stdDuration)
 
+#Combine AUDIOS
+pos1 = 2
+pos2 = 3
+overlayAudio = emptyAudio.overlay(sound1, position=pos1)
+overlayAudio = overlayAudio.overlay(sound2, position=pos2)
 
-annot1 = annotFolder+"annotation_train149.csv"
-annot2 = annotFolder+"annotation_train241.csv"
+# Combine ANNOTATIONS
+annot1 = annotFolder+"149.csv"
+annot2 = annotFolder+"012.csv"
+concatCSV = concatCSV(annot1, annot2, pos1, pos2)
+concatPath = outAnnotFolder+concatCSV
 
-file_handle = combined.export(outFolder+"combined.wav", format="wav")
-file_handle = overlay.export(outFolder+"overlay.wav", format="wav")
+# DATA FRAMES
+df1 = getCSV_DF(annot1)
+df2 = getCSV_DF(annot2)
+dfConcat = getCSV_DF(concatPath)
+
+print(df1)
+print(df2)
+print(dfConcat)
+
+finalPath = outAudioFolder+concatCSV[:-4]+".wav"
+file_handle = overlayAudio.export(finalPath, format="wav")
 
